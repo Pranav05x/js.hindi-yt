8874e6716c589f07599795ce4d2b4b4cd280fa37
Pranav05x <pranavbhoirmoonyt@gmail.com>
2024-12-26 13:54:37 +0000

    p

--- 1.arrays.js
+++ 1.arrays.js
@@ -1,10 +1,10 @@
-// //merging two arrays , arrays will consider whole index as array e.g if 
+//merging two arrays , arrays will consider whole index as array e.g if 
 
-// // when we push values in array
-// // 0 = thor
-// // 1 = iron
-// // 2 = hulk
-// // 3 = ["superman","bat","flash"]
+// when we push values in array
+// 0 = thor
+// 1 = iron
+// 2 = hulk
+// 3 = ["superman","bat","flash"]
 
 
 // const marvel = ["thor","iron","hulk"]
@@ -12,11 +12,11 @@
 // const dc = ["superman","bat","flash"]
 
 //  marvel.push(dc)
-// //  console.log(marvel.includes("hulk")); 
-//  // conclusion : after merging(push) and by using boolean type i get to know array not taking another array`s value
+//  console.log(marvel.includes("hulk")); 
+ // conclusion : after merging(push) and by using boolean type i get to know array not taking another array`s value
  
-// //     //output : superman
-//  // its not goof sentax at all
+//     //output : superman
+ // its not goof sentax at all
 
 const marvel_heros = ["thor", "Ironman", "spiderman"]
 const dc_heros = ["superman", "flash", "batman"]
@@ -29,26 +29,29 @@ const dc_heros = ["superman", "flash", "batman"]
 // const allHeros = marvel_heros.concat(dc_heros)
 // console.log(allHeros);
 
+//concat and sprade oprator both works same
+
 const all_new_heros = [...marvel_heros, ...dc_heros]
+console.log(all_new_heros);
 
-// console.log(all_new_heros);
+// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
-const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
-
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]] 
+// we use flat(infinity) operator to arrange array can also give dept instated of infinity
 const real_another_array = another_array.flat(Infinity)
 console.log(real_another_array);
 
 
 
-console.log(Array.isArray("Hitesh"))
-console.log(Array.from("Hitesh"))
-console.log(Array.from({name: "hitesh"})) // interesting
+// console.log(Array.isArray("Hitesh")) 
+// console.log(Array.from("Hitesh"))
+// console.log(Array.from({name: "hitesh"})) // interesting
 
-let score1 = 100
-let score2 = 200
-let score3 = 300
+// let score1 = 100
+// let score2 = 200
+// let score3 = 300
 
-console.log(Array.of(score1, score2, score3));
+// console.log(Array.of(score1, score2, score3));
 
 
  
