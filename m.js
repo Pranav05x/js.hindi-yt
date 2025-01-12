ffaf4b599a3bf166e0febe8f6dec491c13504bfa
Pranav05x <pranavbhoirmoonyt@gmail.com>
2025-01-12 09:08:15 +0000

    gg

--- m.js
+++ m.js
@@ -3,23 +3,49 @@
 // 1.singleton 
 // 2.object literals 
 
-// Object literals
 
-const mysym = Symbol("key1")
- 
+
+// Object literals***********************************************
+
+const musym = Symbol("key1")
+//can be ased in interview
+//syntax for symbol and it  should  always be declear before usein
+// shuld be define in [symbol]
+
 const jsuser = {
-     name : "pranav",
-    [mysym] : "mykey1",
-    email : "pranav@google.com",
+    name : "pranav",
+    "full name ": "pranavbhoir",
+    [musym] : "mykey1",
     age : 24,
-    location : "mumnbai",
-    isloggedin : false,
-    lasLoginDays : ["monday", "sunday"]
- 
+    email : "pranav@google.com",
+    isLoggedIn : false,
+    lastlogindays : ["monday","saturday"],
+    location : "india"
+
 }
 
-console.log(jsuser.email);    
-console.log(jsuser["email"]);
-console.log(jsuser[mysym]);
+// console.log(jsuser.name);
+// console.log(jsuser["full name "]);
+// console.log(jsuser[musym]);
+
+jsuser.email = "pranavbhoir@gmail.com"
+// Object.freeze(jsuser)
+jsuser.email = "pranav@aws.com"
+// console.log(jsuser);
 
 
+//*****************functions ****************************//
+
+jsuser.greeting = function(){
+    console.log("hello js user");
+    
+}
+jsuser.greeting2 = function(){
+    console.log(`hello js user, ${this.name}`);
+    
+}
+
+console.log(jsuser.greeting()); 
+//just because i freeze the item above, function didint work
+
+console.log(jsuser.greeting2());
