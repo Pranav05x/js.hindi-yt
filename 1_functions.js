de234192df867c9078c96522172dc7f6ae11a756
Pranav05x <pranavbhoirmoonyt@gmail.com>
2025-01-15 09:04:41 +0000

    g

---
+++ 1_functions.js
@@ -0,0 +1,40 @@
+//A function in JavaScript is similar to a procedure—a set of statements that performs a task or calculates a value, but for a procedure to qualify as a function, it should take some input and return an output where there is some obvious relationship between the input and the output
+
+// function_functionname()_function systax
+
+//definition of function
+
+function sayMyName () {
+ console.log("p");
+ console.log("r");
+ console.log("a");
+ console.log("n");
+ console.log("a");
+ console.log("v");
+
+}
+
+//down below there is reference and  exectue
+// sayMyName ()
+
+                       //parameters  
+// function functionname(number1, number2){
+
+//     console.log(number1 + number2);
+   
+
+// }
+
+
+function addTwoNumberse(number1, number2){
+
+   let result = number1 + number2
+   return result
+   
+
+}
+          //arguments
+const result = addTwoNumberse(4,5)
+
+console.log("result", result);
+
