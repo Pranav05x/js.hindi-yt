e878df1a6279c9eb9aa7365a0b810b1eead8a145
Pranav05x <pranavbhoirmoonyt@gmail.com>
2025-01-21 09:25:51 +0000

    k

---
+++ ll.js
@@ -0,0 +1,29 @@
+function calculatecartprice(...number) //we used (rest/sprade) oprator so that we can put multiplevalue
+{
+    return number
+}
+
+// console.log(calculatecartprice(200,400,500,600));
+
+
+const ggg={
+    name :"pranav",
+    email : "god@gmail.com"
+}
+function fname(unpleasent){
+    console.log(`his name is ${unpleasent.name} and its mail id is ${unpleasent.email}`);
+    return
+} 
+
+// console.log(fname(ggg));
+
+// also*********************there is another method
+
+// 2nd method : its not necessary to create object the way we create on top of the code
+
+// fname({
+//     name:"pranav",
+//     email: "god@gmail.com"
+// })
+
+// function with array*************************************************
