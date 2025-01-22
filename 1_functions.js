65dc7e37075a6e339e8aa69b8b2b8ce474414add
Pranav05x <pranavbhoirmoonyt@gmail.com>
2025-01-22 05:34:07 +0000

    l

--- 1_functions.js
+++ 1_functions.js
@@ -61,3 +61,45 @@ function loginUserMessage(username = "pran"){ //we took = pran for e.g
 
 ***************************************************************************
 
+// 2nd topic
+
+function calculatecartprice(...number) //we used (rest/sprade) oprator so that we can put multiplevalue
+{
+    return number
+}
+
+// console.log(calculatecartprice(200,400,500,600));
+
+
+const ggg={
+    name :"pranav",
+    email : "god@gmail.com"
+}
+function fname(unpleasent){
+    console.log(`his name is ${unpleasent.name} and its mail id is ${unpleasent.email}`);
+    return
+} 
+
+// console.log(fname(ggg));
+
+// also*********************there is another method
+
+// 2nd method : its not necessary to create object the way we create on top of the code
+
+// fname({
+//     name:"pranav",
+//     email: "god@gmail.com"
+// })
+
+// function with array*************************************************
+
+
+// const ary = [1,2,3,4]
+function arrayfunction(arrayobj){
+    return arrayobj[2]
+        
+}
+
+// console.log(arrayfunction(ary));
+
+ console.log(arrayfunction([1,2,3,4]));
