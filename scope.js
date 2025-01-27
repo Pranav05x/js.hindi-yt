2c8ade3268639cd8a037532c64b908218f70282d
Pranav05x <pranavbhoirmoonyt@gmail.com>
2025-01-27 09:14:48 +0000

    m

---
+++ scope.js
@@ -0,0 +1,15 @@
+//scope
+
+let a = 100
+
+if(true){
+let a = 1
+const b = 2
+console.log("inner:",a);
+
+}
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// there is diffence in scope from broswer tab(inspect) and node.js
