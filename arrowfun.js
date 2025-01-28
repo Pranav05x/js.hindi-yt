a39e1e5674d0843d1ff755408e7b8cbc63b20b32
Pranav05x <pranavbhoirmoonyt@gmail.com>
2025-01-28 15:00:41 +0000

    j

--- arrowfun.js
+++ arrowfun.js
@@ -4,17 +4,22 @@ const user = {
 
     WelcomeMessage : function(){
         console.log(`${this.username} , welcome to web`);
-        console.log(this);
+        // console.log(this);    (for example)
         
 
      //this means current context, we use this only in {} 
-     //this refers to current context  
+     //this refers to current context   {}  
     }
 
 }
 
-// user.WelcomeMessage()
+// user.WelcomeMessage() 
 // user.username = "sam"
 // user.WelcomeMessage()
 
 console.log(this);
+
+// once upon a time when java used to have only one engine which was in browser
+//but after few years multiple environment came out like node,dino
+//but now we have standalone engine
+ 
