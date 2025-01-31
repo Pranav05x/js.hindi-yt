2c6b9b32a329eda83d2f993c4ea1b80a7648a2ff
Pranav05x <pranavbhoirmoonyt@gmail.com>
2025-01-31 11:05:41 +0000

    k

---
+++ 04_control_flow/one.js
@@ -0,0 +1,20 @@
+//  if statement
+
+const temp = 50
+if (temp === 50 ) {   
+    console.log(`temp is less than 50 `);
+   }   else
+
+{
+       console.log(`temp is grater than 50`);
+}
+
+ // ********************************
+
+ const score = 200 
+
+ if(score < 100){
+        
+ }
+
+
