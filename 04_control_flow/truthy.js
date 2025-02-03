5516e1726192aa8fd667ab32bda874a8aeaac80e
Pranav05x <pranavbhoirmoonyt@gmail.com>
2025-02-03 05:15:38 +0000

    s

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -22,4 +22,27 @@ if (Object.keys(empthyobj).length === 0) {
     console.log(`obj is empty`);
     
 }
+
+// ****************************************************
+// nullish coalescing operator (??) : null undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 10
+val1 = null ?? 10 ?? 20
+// it checks safty value if its null or undefined and if its then it will take another value which was preserved 
+
+console.log(val1);
+// *********************************************************************
+
+// terniary operator
+
+// condition ? true : false
+
+ 
+const iceteaprice = 100
+iceteaprice <= 80 ? console.log(`price is less than 80`) : console.log(`price is greater than 80`);
+
+
  
