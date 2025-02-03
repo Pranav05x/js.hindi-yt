776546cda7909761149e0c7947831dee11197126
Pranav05x <pranavbhoirmoonyt@gmail.com>
2025-02-03 05:45:47 +0000

    s

---
+++ 05_iterations.js/one.js
@@ -0,0 +1,23 @@
+//  for
+
+for (let i = 0; i < 10; i++) {
+    const element = i;
+    if (element === 5) {
+        // console.log(`5 is best`);
+        
+        
+    }
+    // console.log(element);
+
+}
+
+// ********************************************************************
+// Another example of loops in loop
+
+for (let i = 0; i < 10; i++) { 
+     console.log(`outter loop`);
+                                                                              // const element = array[i];  he delete this line while adding 2nd loop,buy why???
+    for (let j = 0; j < 10; j ++) {
+        
+    }
+}
