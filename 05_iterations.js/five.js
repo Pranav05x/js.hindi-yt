db04d75503ec3a997694cc520d333d786dbdbca3
Pranav05x <pranavbhoirmoonyt@gmail.com>
2025-02-04 13:19:12 +0000

    m

---
+++ 05_iterations.js/five.js
@@ -0,0 +1,6 @@
+// ********************************************************
+// sometimes some loops are directly added into property
+// ***********************************************************
+// we can inspect in browser to see its prototpe
+
+const coding = ["js","rb","cpp","python","java"] 
